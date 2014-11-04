d642972e09809f659e1e99a8d17f3ea460be9657
Vanderhaegen Cedrik <cedrik.vanderhaegen@geosolutions.be>
2014-11-04 16:30:55 +0100

    use custom lambert projection def

--- src/MapController.js
+++ src/MapController.js
@@ -33,7 +33,7 @@ define([
         postCreate: function () {
             this.inherited(arguments);
 
-//            proj4.defs('EPSG:31370', "+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.33657,-0.456955,1.84218,-1.2747 +units=m +no_defs");
+            proj4.defs('EPSG:31370', "+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.33657,-0.456955,1.84218,-1.2747 +units=m +no_defs");
 
             var pDef = ol.proj.get('EPSG:3857');
             var pMerc = ol.proj.get('EPSG:900913');
