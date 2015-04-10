da06017be3c6a6de30a719d277f8ca5caaf69387
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-04-10 09:18:33 +0200

    Add crs urn alias to Lambert72 projection

--- MapController.js
+++ MapController.js
@@ -39,6 +39,8 @@ define([
             this.inherited(arguments);
 
             proj4.defs("EPSG:31370","+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.3366,-0.457,1.8422,-1.2747 +units=m +no_defs"); //epsg.io
+						// Add crs urn alias to Lambert72 projection, in order for open layers to recognize it.
+						proj4.defs('urn:ogc:def:crs:EPSG::31370', proj4.defs('EPSG:31370'));
 
             this.pDef = ol.proj.get('EPSG:3857');
             this.pMerc = ol.proj.get('EPSG:900913');
