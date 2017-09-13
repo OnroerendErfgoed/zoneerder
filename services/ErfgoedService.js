3e99da202fe6bf8639753f39294b9e498ff3a344
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2017-09-13 09:19:22 +0200

    reindent code

--- services/ErfgoedService.js
+++ services/ErfgoedService.js
@@ -1,51 +1,51 @@
 define([
-    "dojo/_base/declare",
-    "dijit/_WidgetBase",
-    "dojo/request/xhr",
-    "dojo/_base/array",
-    "dojo/json",
-    "dojo/when"
+  "dojo/_base/declare",
+  "dijit/_WidgetBase",
+  "dojo/request/xhr",
+  "dojo/_base/array",
+  "dojo/json",
+  "dojo/when"
 
 ], function (declare, WidgetBase, xhr, array, JSON, when) {
-    return declare([WidgetBase], {
+  return declare([WidgetBase], {
 
-        url: null,
+    url: null,
 
-        postMixInProperties: function () {
-            this.inherited(arguments);
-        },
+    postMixInProperties: function () {
+      this.inherited(arguments);
+    },
 
-        buildRendering: function () {
-            this.inherited(arguments);
-        },
+    buildRendering: function () {
+      this.inherited(arguments);
+    },
 
-        postCreate: function () {
-            this.inherited(arguments);
-        },
+    postCreate: function () {
+      this.inherited(arguments);
+    },
 
-        startup: function () {
-            this.inherited(arguments);
-        },
+    startup: function () {
+      this.inherited(arguments);
+    },
 
-        searchErfgoedFeatures: function (zone) {
-            if (zone && zone.coordinates.length > 0) {
-                var url = this.url;
-                var data = {
-                    categorie: "aanduidingsobjecten",
-                    geometrie: zone,
-                    'geef_geometrie': 0
-                };
-                return xhr.post(url, {
-									  handleAs:"json",
-                    data: JSON.stringify(data),
-                    headers: {
-                        "X-Requested-With": "",
-											  "Accept": "application/json",
-                        "Content-Type": "application/json"
-                    }
-                });
-            }
-            return when("[]");
-        }
-    });
+    searchErfgoedFeatures: function (zone) {
+      if (zone && zone.coordinates.length > 0) {
+        var url = this.url;
+        var data = {
+          categorie: "aanduidingsobjecten",
+          geometrie: zone,
+          'geef_geometrie': 0
+        };
+        return xhr.post(url, {
+          handleAs:"json",
+          data: JSON.stringify(data),
+          headers: {
+            "X-Requested-With": "",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        });
+      }
+      return when("[]");
+    }
+  });
 });
